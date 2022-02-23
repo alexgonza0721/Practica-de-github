c3a54f856be2c1805d20434c61af29c28c5546ac
Alexender Gonzalez <alexgonve0721@gmail.com>
2022-02-23 16:57:16 -0500

    Agregar archivos de proyecto.

---
+++ Practica de github/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practica_de_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
